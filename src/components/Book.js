33393e5005cca39e502703e35ddb46e4ca457ba1
Ging Gonzalez <ginggonzalez3@gmail.com>
2017-07-30 00:48:12 -0700

    feat: Drag and drop books onto shelves
    Users can now just drag and drop books to move them to their desired
    shelf. This update raises a new warning that says setState can only update
    a mounted component. The specific cause is not known yet. Thus, this issue
    will be resolved in the next commits.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import BookShelfChanger from './BookShelfChanger';
+import { DragDropContainer } from 'react-drag-drop-container';
 
 const Book = ({ book, changeBookShelf }) => (
-  <div className="book">
-    <div className="book-top">
-      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail && book.imageLinks.smallThumbnail}")` }}></div>
-      <BookShelfChanger
-        book={book}
-        changeBookShelf={changeBookShelf}
-      />
+  <DragDropContainer
+    targetKey="book"
+    dragData={{book:book}}
+    returnToBase={true}
+  >
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail && book.imageLinks.smallThumbnail}")` }}></div>
+        <BookShelfChanger
+          book={book}
+          changeBookShelf={changeBookShelf}
+        />
+      </div>
+      <div className="book-title">{book.title && book.title}</div>
+      <div className="book-authors">
+        {book.authors && book.authors.map((author, index) => `${author}${index===book.authors.length-1 ? "" : ", "}`)}
+      </div>
     </div>
-    <div className="book-title">{book.title && book.title}</div>
-    <div className="book-authors">
-      {book.authors && book.authors.map((author, index) => `${author}${index===book.authors.length-1 ? "" : ", "}`)}
-    </div>
-  </div>
+  </DragDropContainer>
 )
 
 export default Book
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,23 +1,29 @@
 import React from 'react'
-
+import { DropTarget } from 'react-drag-drop-container';
+import camelCase from 'camelcase'
 import Book from './Book'
 
 const BookShelf = ({ bookShelfTitle, books, changeBookShelf }) => (
-  <div className="bookshelf">
-    <h2 className="bookshelf-title">{bookShelfTitle}</h2>
-    <div className="bookshelf-books">
-      <ol className="books-grid">
-        {books.map((book) => (
-          <li key={book.id}>
-            <Book
-              book={book}
-              changeBookShelf={changeBookShelf}
-            />
-          </li>
-        ))}
-      </ol>
+  <DropTarget
+    targetKey="book"
+    onHit={(e) => (changeBookShelf(e.dragData.book, camelCase(bookShelfTitle)))}
+  >
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{bookShelfTitle}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book) => (
+            <li key={book.id}>
+              <Book
+                book={book}
+                changeBookShelf={changeBookShelf}
+              />
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
-  </div>
+  </DropTarget>
 )
 
 export default BookShelf
