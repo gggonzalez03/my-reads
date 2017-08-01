1fdc0915b2e39d4481dc1b267fc86ee75fc3da51
Ging Gonzalez <ginggonzalez3@gmail.com>
2017-08-01 10:57:31 -0700

    Revert "feat: Drag and drop books onto shelves"
    Revert master since this feature is moved to a new branch

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,27 +1,20 @@
 import React from 'react'
 import BookShelfChanger from './BookShelfChanger';
-import { DragDropContainer } from 'react-drag-drop-container';
 
 const Book = ({ book, changeBookShelf }) => (
-  <DragDropContainer
-    targetKey="book"
-    dragData={{book:book}}
-    returnToBase={true}
-  >
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail && book.imageLinks.smallThumbnail}")` }}></div>
-        <BookShelfChanger
-          book={book}
-          changeBookShelf={changeBookShelf}
-        />
-      </div>
-      <div className="book-title">{book.title && book.title}</div>
-      <div className="book-authors">
-        {book.authors && book.authors.map((author, index) => `${author}${index===book.authors.length-1 ? "" : ", "}`)}
-      </div>
+  <div className="book">
+    <div className="book-top">
+      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail && book.imageLinks.smallThumbnail}")` }}></div>
+      <BookShelfChanger
+        book={book}
+        changeBookShelf={changeBookShelf}
+      />
     </div>
-  </DragDropContainer>
+    <div className="book-title">{book.title && book.title}</div>
+    <div className="book-authors">
+      {book.authors && book.authors.map((author, index) => `${author}${index===book.authors.length-1 ? "" : ", "}`)}
+    </div>
+  </div>
 )
 
 export default Book
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,29 +1,23 @@
 import React from 'react'
-import { DropTarget } from 'react-drag-drop-container';
-import camelCase from 'camelcase'
+
 import Book from './Book'
 
 const BookShelf = ({ bookShelfTitle, books, changeBookShelf }) => (
-  <DropTarget
-    targetKey="book"
-    onHit={(e) => (changeBookShelf(e.dragData.book, camelCase(bookShelfTitle)))}
-  >
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">{bookShelfTitle}</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {books.map((book) => (
-            <li key={book.id}>
-              <Book
-                book={book}
-                changeBookShelf={changeBookShelf}
-              />
-            </li>
-          ))}
-        </ol>
-      </div>
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{bookShelfTitle}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {books.map((book) => (
+          <li key={book.id}>
+            <Book
+              book={book}
+              changeBookShelf={changeBookShelf}
+            />
+          </li>
+        ))}
+      </ol>
     </div>
-  </DropTarget>
+  </div>
 )
 
 export default BookShelf
