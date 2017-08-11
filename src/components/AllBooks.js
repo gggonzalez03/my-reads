7a014cf483afc53e11bb7b3ccbbb5d8785fb74cc
Ging Gonzalez <ginggonzalez3@gmail.com>
2017-08-11 14:19:07 -0700

    refactor: Loop through available categories
    Categories with book in them will be looped throught to create the
    bookshelves instead of hardcoding them.

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -1,36 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import changeCase from 'change-case'
 
 import BookShelf from './BookShelf'
 
-const AllBooks = ({books, changeBookShelf}) => (
-  <div className="list-books">
+const AllBooks = ({ books, changeBookShelf }) => {
+
+  const categories = [...new Set(books.map((book) => book.shelf))]
+
+  return (<div className="list-books">
     <div className="list-books-title">
       <h1>MyReads</h1>
     </div>
     <div className="list-books-content">
       <div>
-        <BookShelf
-          bookShelfTitle={"Currently Reading"}
-          books={books.filter((book) => book.shelf === "currentlyReading")}
-          changeBookShelf={changeBookShelf}
-        />
-        <BookShelf
-          bookShelfTitle={"Want To Read"}
-          books={books.filter((book) => book.shelf === "wantToRead")}
-          changeBookShelf={changeBookShelf}
-        />
-        <BookShelf
-          bookShelfTitle={"Read"}
-          books={books.filter((book) => book.shelf === "read")}
-          changeBookShelf={changeBookShelf}
-        />
+        {categories.map((category) => (
+          <BookShelf
+            key={category}
+            bookShelfTitle={changeCase.sentenceCase(category)}
+            books={books.filter((book) => book.shelf === category)}
+            changeBookShelf={changeBookShelf}
+          />
+        ))}
       </div>
     </div>
     <div className="open-search">
       <Link to='/addbook'>Add a book</Link>
     </div>
-  </div>
-)
+  </div>)
+}
 
 export default AllBooks
